3215fc5bc9c4367ca74f9310a32a784078a93ca6
kushalnv <120558783+kushalnv@users.noreply.github.com>
2023-03-05 08:23:51 +0530

    Add files via upload

---
+++ dedicated css/App.jsx
@@ -0,0 +1,121 @@
+import React from "react";
+import STYLE from './app.module.css'
+
+const App=()=>{
+    return(
+        <>
+    <nav>
+        <div className={STYLE.ablock}>
+        <ul>
+            <li a href="">Home</li>
+        </ul>
+        <ul>
+            <li a href="">Menu</li>
+        </ul>
+        <ul>
+            <li a href="">options</li>
+        </ul>
+        <ul>
+            <li a href="">About</li>
+        </ul>
+        <ul>
+            <li a href="">Settings</li>
+        </ul>
+        <ul>
+            <li a href="">Help</li>
+        </ul>
+        </div>
+    </nav>
+
+
+<nav>
+<div className={STYLE.bblock}>
+<ul>
+    <li a href="">Home</li>
+</ul>
+<ul>
+    <li a href="">Menu</li>
+</ul>
+<ul>
+    <li a href="">options</li>
+</ul>
+<ul>
+    <li a href="">About</li>
+</ul>
+<ul>
+    <li a href="">Settings</li>
+</ul>
+<ul>
+    <li a href="">Help</li>
+</ul>
+</div>
+</nav>
+
+
+<nav>
+<div className={STYLE.cblock}>
+<ul>
+    <li a href="">Home</li>
+</ul>
+<ul>
+    <li a href="">Menu</li>
+</ul>
+<ul>
+    <li a href="">options</li>
+</ul>
+<ul>
+    <li a href="">About</li>
+</ul>
+<ul>
+    <li a href="">Settings</li>
+</ul>
+<ul>
+    <li a href="">Help</li>
+</ul>
+</div>
+</nav>
+
+
+<nav>
+<div className={STYLE.dblock}>
+<ul>
+    <li a href="">Home</li>
+</ul>
+<ul>
+    <li a href="">Menu</li>
+</ul>
+<ul>
+    <li a href="">options</li>
+</ul>
+<ul>
+    <li a href="">About</li>
+</ul>
+<ul>
+    <li a href="">Settings</li>
+</ul>
+<ul>
+    <li a href="">Help</li>
+</ul>
+</div>
+</nav>
+
+
+
+<nav>
+<div className={STYLE.eblock}>
+<ul>
+    <li a href="">Home</li>
+</ul>
+<ul>
+    <li a href="">Menu</li>
+</ul>
+<ul>
+    <li a href="">options</li>
+</ul>
+
+</div>
+</nav>
+</>
+    )
+}
+export default App
