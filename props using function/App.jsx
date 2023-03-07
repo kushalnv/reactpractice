39acb70090b63ce669e2c4819570ae8d62645d0c
kushalnv <120558783+kushalnv@users.noreply.github.com>
2023-03-07 18:37:01 +0530

    Add files via upload

---
+++ props using function/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Nav from './Nav'
+const App=()=>{
+    return(
+        <Nav data="kushal"/>
+    )
+}
+export default App
---
+++ props using function/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nav=(props)=>{
+    return(
+        <h1>{props.data}</h1>
+
+    )
+}
+export default Nav;
